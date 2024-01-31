92b180a911f9019ac0660a7808dded2789afe4ac
mxike <mike25092001@hotmail.com>
2024-01-31 21:11:32 +0100

    feat: add spider unit tests without working code & add composer.lock to .gitignore

--- app/src/tests/UnitTestsGameUtils.php
+++ app/src/tests/UnitTestsGameUtils.php
@@ -17,7 +17,7 @@ class UnitTestsGameUtils extends TestCase
         $this->game = new Game(self::createStub(DatabaseHandler::class), new GameLogic());
     }
 
-    public function test_Grasshopper_Can_Move_Over_Atleast_One_Tile()
+    public function test_Grasshopper_Must_Move_Over_Atleast_One_Tile()
     {
         // Initialize
         $this->game->setBoard("0,0", "Q");
@@ -105,7 +105,7 @@ class UnitTestsGameUtils extends TestCase
         self::assertEquals('Move would split hive', $this->game->getError()); // if ant tries to go to a place without neighbours it would split the hive.
     }
 
-    public function test_Ant_Can_Move_To_Tile_With_Neighbours()
+    public function test_Ant_Must_Move_To_Tile_With_Neighbours()
     {
         // Initialize
         $this->game->setBoard("0,0", "Q");
@@ -120,5 +120,73 @@ class UnitTestsGameUtils extends TestCase
 
         // Assert
         self::assertEquals('', $this->game->getError());
+    }
+
+    public function test_Spider_Can_Not_Move_To_Itself()
+    {
+        // Initialize
+        $this->game->setBoard("0,0", "Q");
+        $this->game->setBoard("0,-1", "S");
+        $this->game->setPlayer(1); // set player to black
+        $this->game->setBoard("0,1", "Q");
+        $this->game->setBoard("0,2", "B");
+        $this->game->setPlayer(0); // set player to white
+
+        // Action
+        $this->game->move("0,-1", "0,-1");
+
+        // Assert
+        self::assertEquals('Spider cannot move to this position', $this->game->getError());
+    }
+
+    public function test_Spider_Can_Not_Move_To_Tile_Without_Neighbours()
+    {
+        // Initialize
+        $this->game->setBoard("0,0", "Q");
+        $this->game->setBoard("0,-1", "S");
+        $this->game->setPlayer(1); // set player to black
+        $this->game->setBoard("0,1", "Q");
+        $this->game->setBoard("0,2", "B");
+        $this->game->setPlayer(0); // set player to white
+
+        // Action
+        $this->game->move("0,-1", "-2,5");
+
+        // Assert
+        self::assertEquals('Move would split hive', $this->game->getError());
+    }
+
+    public function test_Spider_Must_Move_To_Tile_Without_Neighbours()
+    {
+        // Initialize
+        $this->game->setBoard("0,0", "Q");
+        $this->game->setBoard("0,-1", "S");
+        $this->game->setPlayer(1); // set player to black
+        $this->game->setBoard("0,1", "Q");
+        $this->game->setBoard("0,2", "B");
+        $this->game->setPlayer(0); // set player to white
+
+        // Action
+        $this->game->move("0,-1", "1,1");
+
+        // Assert
+        self::assertNull($this->game->getError());
+    }
+
+    public function test_Spider_Must_Move_Three_Steps()
+    {
+        // Initialize
+        $this->game->setBoard("0,0", "Q");
+        $this->game->setBoard("0,-1", "S");
+        $this->game->setPlayer(1); // set player to black
+        $this->game->setBoard("0,1", "Q");
+        $this->game->setBoard("0,2", "B");
+        $this->game->setPlayer(0); // set player to white
+
+        // Action
+        $this->game->move("0,-1", "-1,1");
+
+        // Assert
+        self::assertNull($this->game->getError());
     }
 }
